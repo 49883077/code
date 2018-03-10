ee6cfd0885b81425b0862b5c765eee4379958e7f
49883077 <498837077@qq.com>
2018-03-10 09:42:58 +0800

    添加客户端代码

---
+++ client/assets/Script/HelloWorld.js
@@ -0,0 +1,23 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        label: {
+            default: null,
+            type: cc.Label
+        },
+        // defaults, set visually when attaching this script to the Canvas
+        text: 'Hello, World!'
+    },
+
+    // use this for initialization
+    onLoad: function () {
+        this.label.string = this.text;
+        this.label.string = "Hello World3"
+    },
+
+    // called every frame
+    update: function (dt) {
+
+    },
+});
